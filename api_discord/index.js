ae807a559af8b3793622695e918a6179aefc9757
kcomain <cheunghowui@gmail.com>
2019-03-20 17:08:25 +0800

    3 files modified

--- api_discord/index.js
+++ api_discord/index.js
@@ -2,4 +2,16 @@
     These programs are written by Kenny Cheung. 
     PLEASErefer to me if you are going to use these programs.
 */
-const 
+
+
+// Check for the various File API support.
+if (window.File && window.FileReader && window.Blob) {
+  alert('All API supported YAY!')
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
+
+
+let versionNumber=Number(1)
+const appDiv = document.getElementById('version');
+appDiv.innerHTML = versionNumber;
--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 import './style.css';
 
 // Write Javascript code!
+/*
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+*/
+let versionNumber=Number(1)
+const appDiv = document.getElementById('version');
+appDiv.innerHTML = versionNumber;
