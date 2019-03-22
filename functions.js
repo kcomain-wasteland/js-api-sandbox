1de62f8e267b4b1e6f04437cb19a196c24b68358
Kenny <cheunghowui@gmail.com>
2019-03-22 11:59:14 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,10 @@
-function setHtmlValue(id,value){
+export function setHtmlValue(id,value){
   document.getElementById(id).setInnerHTML(value);
   return
+}
+//Define commands and functions as below.
+export function foo(bar){
+  const foo="foo"
+  const bar="bar"
+  const par=bar
 }
