794e0c83299a458d4f91b6bf466e7621bd341951
kcomain <cheunghowui@gmail.com>
2019-03-25 12:25:37 +0800

    1 file modified, 1 added, 1 deleted

---
+++ functions.mjs
@@ -0,0 +1,30 @@
+export function setHtmlValue(id,value){
+  document.getElementById(id).setInnerHTML(value);
+  return
+}
+//Define commands and functions as below.
+export function foo(bar){
+  const foo="foo"
+  const bar="bar"
+  const par=bar
+}
+
+export function webhook(mode,url,variable){          //Proven Work
+  const http = new XMLHttpRequest();
+  http.open(mode, url);
+  if (mode!="GET"){
+    var json = prompt("JSON Data")
+    var header = prompt("Content Type(application/json)")
+    http.setRequestHeader("Content-Type",header+";charset=UTF-8" )
+    http.send(JSON.stringify(json));
+    console.log(http.responseText)
+    return http.responseText
+  } else {
+  http.send();
+    http.onreadystatechange=(e)=>{
+      console.log(http.responseText)
+      variable = http.responseText
+      return
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 // Import stylesheets
 import './style.css';
-import './functions.js'
+import './functions.mjs'
 import 'jquery'
 // Write Javascript code!
+/*
 function setHtmlValue(id,value){     //Proven Work
   document.getElementById(id).innerHTML = value;
   return
 }
-function webhook(mode,url,variable){          //Proven Work
-  const http = new XMLHttpRequest();
-  http.open(mode, url);
-  if (mode!="GET"){
-    var json = prompt("JSON Data")
-    var header = prompt("Content Type(application/json)")
-    http.setRequestHeader("Content-Type",header+";charset=UTF-8" )
-    http.send(JSON.stringify(json));
-    console.log(http.responseText)
-    return http.responseText
-  } else {
-  http.send();
-    http.onreadystatechange=(e)=>{
-      console.log(http.responseText)
-      variable = http.responseText
-      return
-    }
-  }
-}
+*/
 
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
@@ -33,3 +16,5 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 //Time to have a rest......(2019/03/20)
 //Finally, can use WebHooks.
  setHtmlValue("version",9);
+
+
